375c4bf54a4181573f55dbf28adf8187dc2145e1
Burcu Gunes <burcudagli.10@gmail.com>
2025-11-16 17:03:03 +0000

    initial server.js file added

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const sequelize = require("./config/connection");
+const userRoutes = require("./routes/user");
+const blogRoutes = require("./routes/blog");
+const categoryRoutes=require("./routes/category")
+const path = require("path");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/blogs", blogRoutes);
+
+
+const PORT = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, "public")));
+
+const rebuild = process.argv[2] === "--rebuild";
+
+sequelize.sync({ force: rebuild }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT} `));
+});
