45448cfec8fcbfaf7921ea8ed391705df09d8aa3
Burcu Gunes <burcudagli.10@gmail.com>
2025-11-16 16:55:15 +0000

    Sequelize model relationships added

---
+++ models/index.js
@@ -0,0 +1,29 @@
+// import all models
+const Category = require("./category");
+const User = require("./user");
+const Blog = require("./blog");
+
+Blog.belongsTo(Category, {
+  foreignKey: "categoryId",
+  as: "category",
+});
+
+Category.hasMany(Blog, {
+  foreignKey: "categoryId",
+  as: "blogs",
+});
+
+User.hasMany(Blog, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Blog.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+module.exports = {
+  Blog,
+  Category,
+  User,
+};
