211023532a4677bbfb7b562751fe2864c9fb6e35
Burcu Gunes <burcudagli.10@gmail.com>
2025-11-16 14:45:57 +0000

    auth added

---
+++ utils/auth.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.JWT_SECRET || 'secrett!!';
+const expiration = '2h';
+
+const authMiddleware = (req, res, next) => {
+
+  let token = req.headers.authorization;
+
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
+
+  if (!token) {
+    res.status(400).json({ message: 'Bearer Token not supplied or invalid' });
+    return;
+  }
+
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+    next();
+  } catch (err) {
+    console.log('Invalid token');
+    res.status(400).json({ message: 'Invalid token: ' + err.message });
+  }
+
+  return req;
+}
+
+const signToken = (user) => {
+
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
+
+module.exports = { authMiddleware, signToken };
